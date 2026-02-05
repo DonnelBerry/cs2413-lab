f594053aab0b2af2f1e814cc2fe2f039c0126d0f
root <root@KannasComputer.localdomain>
2026-02-05 09:38:00 -0600

    lab01: complete

--- lab01/src/two_sum.c
+++ lab01/src/two_sum.c
@@ -1,7 +1,15 @@
 #include "two_sum.h"
 
 int two_sum(const int* nums, int n, int target, int* out_i, int* out_j) {
-
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if (nums[i] + nums[j] == target){
+                *out_i = i;
+                *out_j = j;
+                return 1;
+            }
+        }
+    }
     // TODO: implement Two Sum.
     // Requirements:
     //  - find i < j such that nums[i] + nums[j] == target
